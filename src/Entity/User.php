af65f44bbf814ce63b1c19ad0b106b6633febf0d
unknown <aziyade.cornet@gmail.com>
2021-05-12 12:20:40 +0200

    ajout sur le pc de aziyade

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,7 +39,7 @@ class User implements UserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=false)
      */
     private $nom;
 
