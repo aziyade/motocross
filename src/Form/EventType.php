0746fa7baaeeb1766875d6e40aa980e0a93712c8
Tevin <tevin.abhay@gmail.com>
2021-05-12 14:26:57 +0200

    ajout de placeholder à date_evenement et use datetimetype

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,7 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class EventType extends AbstractType
 {
@@ -15,7 +16,12 @@ class EventType extends AbstractType
             ->add('type')
             ->add('titre')
             ->add('description')
-        ;
+            ->add('date_evenement', DateTimeType::class, [
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                    'hour' => 'Heure', 'minute' => 'Minute', 'second' => 'Seconde',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
