75f19e61024accabd958addc4b4ed9025462451e
aziyade <aziyade.cornet@gmail.com>
2021-06-07 16:14:00 +0200

    admin et bouton logout

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+        /**
+     * @Route("/admin/add-event", name="admin_add_event")
+     */
+    public function addEvent(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
