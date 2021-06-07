23ef70a9c9e9d95c8be4b006f6be6e1f89ea2580
aziyade <aziyade.cornet@gmail.com>
2021-06-07 17:01:11 +0200

    lien vers bdd pour listes users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,8 +24,31 @@ class AdminController extends AbstractController
      */
     public function addEvent(): Response
     {
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/add_event.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+            /**
+     * @Route("/admin/add-user", name="admin_add_user")
+     */
+    public function addUser(): Response
+    {
+        
+        
+        return $this->render('admin/add_user.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+               /**
+     * @Route("/admin/liste-user", name="admin_liste_user")
+     */
+    public function listeUser(UserRepository $repo): Response
+    {
+        
+        
+        return $this->render('admin/liste_user.html.twig', [
+            'users' => $repo->findAll(),
         ]);
     }
 }
